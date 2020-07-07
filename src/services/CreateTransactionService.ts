e26497236b3ff7f27d293f0fa2719aa1021b57cf
Ariel Kollross <arielkollross@gmail.com>
2020-07-07 16:26:38 -0300

    migrations created

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,38 @@
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+  date: Date;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+    date,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category,
+      created_at: date,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
